f71ddecb877c4778c7ad2e02f17b976512af089b
Misha <Misha@MISHAS_PC>
2021-09-25 18:51:06 -0400

    Add project files.

---
+++ WordUnscrambler/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class FileReader
+    {
+        public string[] Read(string filename)
+        {
+            // Implement this.
+
+            return null;
+
+        }
+    }
+}
---
+++ WordUnscrambler/WordMatcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class WordMatcher
+    {
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        {
+            List<MatchedWord> matchedWords = new List<MatchedWord>();
+
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach (var word in wordList)
+                {
+                    //scrambledWord already matches word
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase)) {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+
+            MatchedWord BuildMatchedWord(string scrambledWord, string word)
+            {
+                MatchedWord matchedWord = new MatchedWord
+                {
+                    ScrambledWord = scrambledWord,
+                    Word = word
+                };
+
+                return matchedWord;
+            }
+
+            return matchedWords;
+        } 
+    }
+
+}
